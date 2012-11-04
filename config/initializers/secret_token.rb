c0b7268ed4ed7909d0da0fe9163c945e33d27754
Andrew Lemmons <andrew@medalkore.com>
2012-11-04 01:51:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedalKore::Application.config.secret_token = 'e0eafc30dfd783f4ed26f1778ff00dd71094fa359717685944de1021a7ed03078a4240681d389e151ababb55891020554d6da2b7e89f8a92d5c73dd449643f88'
